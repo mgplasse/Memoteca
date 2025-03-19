e298b9d4cfdec039052257dfa5aa122c7e8690f9
mgplasse <103143829+mgplasse@users.noreply.github.com>
2025-03-19 17:13:25 -0300

    Update  box-shadow menu pensamentos

---
+++ files/script-memoteca.js
@@ -0,0 +1,7 @@
+function collapsePensamento() {
+    var menuPensamento = document.getElementsByTagName('button-pensamento')
+    create
+
+}
+
+// onclick sumir o menu de adicionar pensamento. e collapsar o menu de escrita contendo nome de autor + pensamento
